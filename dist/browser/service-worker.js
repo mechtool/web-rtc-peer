9640580c8644d5ecc5d0d6331c42cb15e6e14886
Dell <farros@rambler.ru>
2020-01-08 14:10:26 +0300

    make it better

---
+++ dist/browser/service-worker.js
@@ -0,0 +1,26 @@
+importScripts('ngsw-worker.js');
+let version = '1.0.0';
+
+/*self.addEventListener('push', (event)=> {
+	const data = event.data.json();
+	let title = data.notification.title +' от ' + data.user.uid,
+		options = {
+		/!*	actions : data.notification.actions,*!/
+			body : data.notification.body,
+		} ;
+	event.waitUntil(self.registration.showNotification(title, options));
+});*/
+
+//Обработка событина нажатия пользователем на уведомление
+/*self.addEventListener('notificationclick', function(event) {
+	if (event.action === 'archive') {
+		// Archive action was clicked
+		console.log('Нажата кнопка активности.')
+	} else {
+		// Main body of notification was clicked
+		console.log('Нажато тело сообщения.');
+		self.clients.openWindow('/application/announcements');
+	}
+	event.notification.close();
+	
+}, false);*/
