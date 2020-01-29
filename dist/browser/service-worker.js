37b9122913851b09a835690f978e8ae1ee8fcfb1
Dell <farros@rambler.ru>
2020-01-29 17:28:46 +0300

    make it better

--- dist/browser/service-worker.js
+++ dist/browser/service-worker.js
@@ -1,6 +1,6 @@
 importScripts('ngsw-worker.js');
 
-let version = '1.0.02';
+let version = '1.0.011';
 let channel = new BroadcastChannel('sw-messages');
 channel.addEventListener('message', event => {
 	if(event.data.type === 'version'){
