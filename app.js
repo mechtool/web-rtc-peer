ac374b4e755ed065c3fe1eef0892659231b91207
Dell <farros@rambler.ru>
2020-03-02 12:48:20 +0300

    make it better

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var path = require('path');
 var bodyParser = require('body-parser') ;
 var compression = require('compression');
 var debug = require('debug')('stun-turn-heroku:server');
-var http = require('http');
+var https = require('https');
 var Turn = require('node-turn');
 var cors = require('cors');
+var querystring = require('querystring');
 const webPush = require('web-push');
 const pushApiKeys = require( './dist/browser/assets/js/apiKeys.json');
 const {AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap} = require('./dist/server/main');
@@ -73,6 +74,7 @@ var server = http.createServer(app);
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
+getMoneyToken();
 
 app.get('*.*', express.static(DIST_FOLDER, {
 	maxAge: '1y',
@@ -86,6 +88,36 @@ app.get('*.*', express.static(DIST_FOLDER, {
 		}
 	}
 }));
+
+function getMoneyToken(){
+	// Build the post string from an object
+		var post_data = querystring.stringify({
+			'grant_type' : 'authorization_code',
+			'client_id' : 'D1092504AB7FEA4F571EE94C162AE767BD781CFB1779B275670FBBD39EE7D2F7',
+			'client_secret' :'0BB24041B50B784FB17FF033C05BD46D93B566BCD89433B39F861A94AD50150A2536FF4D0CB7B6AC1C953D14A4473119181BFE0738DA3D77E040CE4BE715917B'
+		});
+		
+		// An object of options to indicate where to post to
+		var post_options = {
+			host: 'money.yandex.ru',
+			path: '/oauth/authorize',
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				'Content-Length': Buffer.byteLength(post_data)
+			}
+		};
+		// Set up the request
+		var post_req = https.request(post_options, function(res) {
+			res.setEncoding('utf8');
+			res.on('data', function (chunk) {
+				console.log('Response: ' + chunk);
+			});
+		});
+		// post the data
+		post_req.write(post_data);
+		post_req.end();
+}
 //Уведомление о принятом платеже
 app.post('/payment-notification', (req, resp)=>{
 	  if(!req.body['test_notification']){
@@ -94,9 +126,8 @@ app.post('/payment-notification', (req, resp)=>{
 			add = parseFloat(amount) + parseFloat(mess[2]);
 	  	//uid/amount/current/date(milliseconds)
 		  console.log(mess);
-	  	  db.ref(`/sms/${mess[0]}/${mess[3]}`).set(add);
+	  	  db.ref(`/sms/${mess[0]}}`).set(add);
 	  }
-	  console.log(req.body);
 	  resp.status(200).end();
 });
 app.post('/notification', (req, resp)=>{
